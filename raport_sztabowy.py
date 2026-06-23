a8af24829328dd46dd6522c6f792d2c82a300367
Jacek9712 <j.mackowiak97@gmail.com>
2026-06-23 10:37:36 +0200

    Add 'Jakub Kendzia' and 'Karol Łysiak' to player list

--- raport_sztabowy.py
+++ raport_sztabowy.py
@@ -19,10 +19,10 @@ GODZINA_WELLNESS = 10
 GODZINA_RPE = 17
 
 LISTA_ZAWODNIKOW = sorted([
-    "Bartosz Piechowiak", "Bartosz Wiktoruk", "Dima Avdieiev", "Filip Jakubowski", 
-    "Filip Tonder", "Filip Waluś", "Igor Kornobis", "Iwo Wojciechowski", 
+    "Bartosz Lelito", "Bartosz Piechowiak", "Bartosz Wiktoruk", "Dima Avdieiev", "Filip Jakubowski", 
+    "Filip Tonder", "Filip Waluś", "Igor Kornobis", "Iwo Wojciechowski", "Jakub Kendzia", 
     "Jakub Kosiorek", "Jan Niedzielski", "Kacper Lepczyński", "Kacper Rychert", 
-    "Kacper Szymanek", "Kamil Kumoch", "Karol Dziedzic", "Leo Przybylak", 
+    "Kacper Szymanek", "Kamil Kumoch", "Karol Dziedzic", "Karol Łysiak", "Leo Przybylak", 
     "Marcel Stefaniak", "Marcel Zylla", "Mateusz Stanek", "Michał Smoczyński", 
     "Patryk Kusztal", "Paweł Kwiatkowski", "Oskar Mazurkiewicz", "Sebastian Steblecki", 
     "Szymon Michalski", "Szymon Zalewski", "Tomasz Wojcinowicz"

b23e6ff93079395d5072eaf68dd55d9c5d700e1d
Jacek9712 <j.mackowiak97@gmail.com>
2026-09-08 08:07:27 +0200

    Add 'Filip Rejczyk' to the group list

--- raport_sztabowy.py
+++ raport_sztabowy.py
@@ -28,7 +28,7 @@ FALLBACK_LISTA_ZAWODNIKOW = sorted([
     "Kacper Lepczyński", "Kacper Rychert", 
     "Karol Łysiak", "Leo Przybylak", "Marcel Stefaniak", "Marcel Zylla", 
     "Mateusz Stanek", "Michał Smoczyński", "Patryk Kusztal", "Paweł Kwiatkowski", 
-    "Oskar Mazurkiewicz", "Sebastian Steblecki", "Szymon Zalewski", "Tomasz Wojcinowicz", "Aleksander Wołczek", "Jakub Apolinarski", "Arkadiusz Najemski", "Oleksandr Azatskyi", "Mikołaj Baran", "Antoni Młynarczyk", "Kacper Wybieralski", "Karol Kalata"
+    "Oskar Mazurkiewicz", "Sebastian Steblecki", "Szymon Zalewski", "Tomasz Wojcinowicz", "Aleksander Wołczek", "Jakub Apolinarski", "Arkadiusz Najemski", "Oleksandr Azatskyi", "Mikołaj Baran", "Antoni Młynarczyk", "Kacper Wybieralski", "Karol Kalata", "Filip Rejczyk"
 ])
 
 FALLBACK_GRUPY_LISTA = [
